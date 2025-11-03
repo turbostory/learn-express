830a7a2a6e6fc106de921f95e7cbef2195ad9d0a
turbostory <tstory@gmail.com>
2025-11-03 14:16:21 +0900

    end추가

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express"); // express 가져오기
+const dotenv = require("dotenv");
+
 const app = express(); // express 함수 호출
+
+// 환경변수 로드
+dotenv.config();
 
 // buiness logic이 들어감
 
 // 서버 실행 (js 코드의 가장 하단에 위치)
 app.listen(8080, () => {
-    console.log("Server running");
+    console.log("Server running", process.env.PORT);
 })
